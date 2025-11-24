51686bae56fc63d2d36abae2fa915c58c3ba58cd
Lucas de Bitencourt Frasson <lucasb.frasson@gmail.com>
2025-11-24 10:56:35 -0300

    feat: connecting to the postgres

--- api/src/main/java/cutepush/beanorganized/api/BeanOrganizedApplication.java
+++ api/src/main/java/cutepush/beanorganized/api/BeanOrganizedApplication.java
@@ -2,11 +2,15 @@ package cutepush.beanorganized.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
 @SpringBootApplication(scanBasePackages = {
     "cutepush.beanorganized.api",
     "cutepush.beanorganized.domain"
 })
+@EnableJdbcRepositories(basePackages = "cutepush.beanorganized.domain.repository")
+@EntityScan(basePackages = "cutepush.beanorganized.domain.entity")
 public class BeanOrganizedApplication {
 
     public static void main(String[] args) {
