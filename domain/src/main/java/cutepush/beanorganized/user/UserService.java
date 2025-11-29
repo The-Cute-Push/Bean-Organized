4f1bebff9a8fb5ff3a643043c1fb98710c57cc06
Lucas de Bitencourt Frason <lucasb.frasson@gmail.com>
2025-11-29 10:58:37 -0300

    feat: Rest of the crud + JPA implementation + Tests

--- domain/src/main/java/cutepush/beanorganized/user/UserService.java
+++ domain/src/main/java/cutepush/beanorganized/user/UserService.java
@@ -2,17 +2,30 @@ package cutepush.beanorganized.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    private final UserRepository repo;
+    private final UserRepository userRepository;
 
     public UserEntity findByName(String name) {
-        return repo.findByName(name);
+        return userRepository.findByName(name);
     }
 
     public UserEntity save(UserEntity userEntity) {
-        return repo.save(userEntity);
+        return userRepository.save(userEntity);
+    }
+
+    public Optional<UserEntity> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public Iterable<UserEntity> findAll() {
+        return userRepository.findAll();
+    }
+
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
     }
 }
